ee6cf5245e8ce73a772b199e27511e434e0c548b
Travis <tzoch2008@gmail.com>
2013-10-26 05:42:53 -0400

    Added a utility file to delete temporary files.

--- debug.py
+++ debug.py
@@ -2,8 +2,10 @@
 
 '''Some utility and debugging functions for the Dropbox Bot'''
 
-import time
 import hashlib
+import logging
+import os
+import time
 
 def generate_thing_ids(n):
     '''
@@ -25,6 +27,23 @@ def generate_thing_ids(n):
         output.append(thing_id(i))
 
     return output
+
+def delete_tmp_files(directory):
+
+    files_to_delete = os.listdir(directory)
+    i = 0
+    t = time.strftime('%H:%M:%S')
+
+    if files_to_delete:
+        for tmp_file in files_to_delete:
+            os.remove(directory + '/' + tmp_file)
+            i += 1
+
+        logging.info('[%s] Temporary Files Clear! %s files deleted', t, i)
+        return
+    else:
+        logging.info('[%s] Temporary Files Clear! No Files to delete', t) 
+        return
 
 if __name__ == '__main__':
     print generate_thing_ids(5)
