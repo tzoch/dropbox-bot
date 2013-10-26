2e1fa3c96c4750689cc1e8fe32dd512dc4034ba6
Travis <tzoch2008@gmail.com>
2013-10-26 05:50:38 -0400

    Added function to download and save dropbox links. Added beginnings of logging.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #! usr/bin/python
 
-import time
 import json
 import logging
+import os
+import time
 
 import urlparse as up
 
@@ -11,8 +12,7 @@ import praw
 import pyimgur
 
 from database import Database
-
-supported_extensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp']
+from debug import delete_tmp_files
 
 def get_direct_dl_link(url):
     '''
@@ -24,6 +24,24 @@ def get_direct_dl_link(url):
     
     return 'http://dl.dropboxusercontent.com' + up.urlparse(url).path
 
+def download_file(url):
+    accepted_content_types = ['image/jpeg',
+                              'image/png',
+                              'image/gif',
+                              'image/apng'
+                             ]
+
+    dropbox_link = get_direct_dl_link(url)
+
+    r = requests.get(dropbox_link)
+
+    if r.headers['content-type'] in accepted_content_types:
+        # Download the link from dropbox (dropbox_link) and rename it to
+        # a temporary file. Preserve the file extension.
+        tmp_filename = 'tmp' + os.path.splitext(dropbox_link)[1]
+	f = open('tmp/' + tmp_filename, 'wb')
+        f.write(r.content)
+        f.close()
 
 def main():
     config = json.load(open('config.json'))
@@ -31,11 +49,16 @@ def main():
     r = praw.Reddit(config['user-agent'])
     r.login(config['username'], config['password'])
     submissions = r.get_domain_listing('dropbox.com', sort='new', limit=2)
-    
-    for submission in submissions:
-        print vars(submission)
+
+    #for submission in submissions:
+    url = 'https://www.dropbox.com/TESTURL.jpg'
+ 
+    # commented out for testing purposes
+    #download_file(url) 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='dropbox-bot.log', level=logging.DEBUG)
+
     print 'Started Main'
-    main()
+    delete_tmp_files('tmp') # eventually place this at end of main loop
     print 'Finished Main'
