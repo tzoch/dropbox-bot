5ce7209215627acf03723c4fcdba47469c6ced1c
Travis <tzoch2008@gmail.com>
2013-10-27 21:44:33 -0400

    Started class that will handle the bot's intereactions with reddit.

---
+++ reddit.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import time
+import logging
+
+import praw
+
+from database import Database
+
+class RedditBot(object):
+
+    def __init__(self):
+        pass
+
+    def get_submissions(self):
+        pass
