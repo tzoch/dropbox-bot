a42db7a7e972c827edaa5c7ba593e5be0c969ed6
Travis <tzoch2008@gmail.com>
2013-10-31 23:46:51 -0400

    Added ability to monitor reddit and add a comment to the original submissions when a dropbox link meets the bots requirements. Still need to add error checking and calls to the database to ensure the bot doesn't double post. Also need to loop the entire bot so it can keep monitoring the dropbox domain.

--- main.py
+++ main.py
@@ -15,20 +15,48 @@ from database import Database
 from utils import delete_tmp_files
 from dropbox import DropBox
 
+def build_comment(imgur_url):
+    head = '''Hi! I noticed that you posted an image from dropbox.com. I have
+            rehosted your image to imgur because high traffic can break 
+            dropbox.com links.\n\n'''
+
+    body = '[Here is the rehosted image](' + imgur_url + ')\n\n'
+
+    tail = '''This action was performed by a bot. If there is an issue or
+            problem, please report it below.\n\n'''
+
+    foot = '''[^[DropBox_Bot&nbsp;FAQ]](http://www.reddit.com/r/DropBox_Bot/wiki/faq) 
+        [^[Feedback]](http://www.reddit.com/r/DropBox_Bot/submit) 
+        [^[Source]](https://github.com/tzoch/dropbox-bot)''' 
+
+    return head + body + tail + foot
+
 def main():
     config = json.load(open('config.json'))
+    db = Database(config['database'])
 
     r = praw.Reddit(config['user-agent'])
     r.login(config['username'], config['password'])
-    submissions = r.get_domain_listing('dropbox.com', sort='new', limit=2)
+    #submissions = r.get_domain_listing('dropbox.com', sort='new', limit=2)
+    # switch the comment out when the bot goes live
+    submissions = r.get_subreddit('DropBox_Bot').get_new(limit=2)
 
     for submission in submissions:
-        print submission.name
- 
+        if not db.is_processed(submission.name):
+            drop = DropBox(submission.url) 
+            drop.download_file()
+            imgur_url = drop.rehost_image()
+
+            if imgur_url:
+                comment = build_comment(imgur_url)
+                submission.add_comment(comment)
+
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='dropbox-bot.log', level=logging.DEBUG)
+    logging.basicConfig(filename='dropbox-bot.log', 
+                       # format='[%(asctime)-15s] %(levelname)-8s : %(message)',
+                        level=logging.DEBUG)
 
     print 'Started Main'
     main()
     print 'Finished Main'
-
